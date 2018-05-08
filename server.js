04dc4400b45b0ce8fed64acb652d0e2391007c47
Limbert Bontigao <limbert08@gmail.com>
2018-05-08 18:21:28 -0400

    made PORT dynamic

--- server.js
+++ server.js
@@ -74,7 +74,9 @@ app.get("/weight", function(req, res) {
   });
 });
 
+var PORT = process.env.PORT || 3000;
+
 // Set the app to listen on port 3000
-app.listen(3000, function() {
+app.listen(PORT, function() {
   console.log("App running on port 3000!");
 });
